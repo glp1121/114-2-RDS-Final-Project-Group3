74fd739a53181028f3656f90d83de085855e87ee
翊茗 洪 <110208029@g.nccu.edu.tw>
2026-06-07 16:21:22 +0800

    add slides

--- R/Final Project - Model Analysis.R
+++ R/Final Project - Model Analysis.R
@@ -1,8 +1,15 @@
 library(dplyr)
 library(modelsummary)
 library(performance)
-
 mrt_weather_clean <- readRDS("Output/mrt_weather_clean.rds")
+#檢查相關問題
+mrt_weather_clean %>%
+  select(rain_dummy,
+         temperature,
+         humidity,
+         weekend) %>%
+  cor(use = "complete.obs")
+
 
 # Model0: Weather only
 m0 <- lm(
@@ -13,6 +20,7 @@ m0 <- lm(
     humidity,
   data = mrt_weather_clean
 )
+summary(m0)
 
 # Model 1: Weather + Time
 m1 <- lm(
@@ -25,6 +33,7 @@ m1 <- lm(
     factor(hour),
   data = mrt_weather_clean
 )
+summary(m1)
 
 # Model 2: Weekend × Hour
 #檢查平日與假日的日內需求型態是否不同
@@ -38,7 +47,7 @@ m2 <- lm(
     weekend * factor(hour),
   data = mrt_weather_clean
 )
-
+summary(m2)
 # Model 3: Rain × Hour
 # 檢查雨天效果是否因時段不同而異
 
@@ -51,6 +60,7 @@ m3 <- lm(
     weekend,
   data = mrt_weather_clean
 )
+summary(m3)
 
 # Model 4: Full model with MRT station fixed effects
 #控制不同捷運站本身的需求差異
@@ -66,27 +76,27 @@ m4 <- lm(
     factor(mrt_station),
   data = mrt_weather_clean
 )
+summary(m4)
+
+# Model 5: Rain × Weekend
+m5 <- lm(
+  trip_count ~
+    rain_dummy * weekend +
+    temperature +
+    I(temperature^2) +
+    humidity +
+    factor(hour) +
+    factor(mrt_station),
+  data = mrt_weather_clean
+)
+summary(m5)
 
 # Model performance comparison
 model_performance <- compare_performance(
-  m0, m1, m2, m3, m4
+  m0, m1, m2, m3, m4, m5
 )
 model_performance
 
-
-# 3. Nested model tests
-
-# 時間變數是否顯著改善模型？
-anova(m0, m1)
-
-# 平日/假日的日內型態是否不同？
-anova(m1, m2)
-
-# 雨天效果是否因時段不同而異？
-anova(m1, m3)
-
-# 站點固定效果是否重要？
-anova(m1, m4)
 
 # 4. Export model comparison table
 modelsummary(
@@ -95,7 +105,8 @@ modelsummary(
     "M1" = m1,
     "M2" = m2,
     "M3" = m3,
-    "M4" = m4
+    "M4" = m4,
+    "M5" = m5
   ),
   coef_omit = "factor\\(hour\\)|factor\\(mrt_station\\)",
   output = "Tables/regression.png"
