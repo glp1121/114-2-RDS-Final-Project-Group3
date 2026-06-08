070ab060b32461eea60b14c4dc15283170f89998
翊茗 洪 <110208029@g.nccu.edu.tw>
2026-06-08 11:45:20 +0800

    add nb and station fixed effect

--- R/Final Project - Model Analysis.R
+++ R/Final Project - Model Analysis.R
@@ -1,6 +1,8 @@
 library(dplyr)
 library(modelsummary)
 library(performance)
+library(broom)
+library(ggplot2)
 mrt_weather_clean <- readRDS("Output/mrt_weather_clean.rds")
 #檢查相關問題
 mrt_weather_clean %>%
@@ -111,3 +113,40 @@ modelsummary(
   coef_omit = "factor\\(hour\\)|factor\\(mrt_station\\)",
   output = "Tables/regression.png"
 )
+
+#
+station_fe <- tidy(m4) %>%
+  filter(grepl("factor\\(mrt_station\\)", term)) %>%
+  mutate(
+    station = gsub("factor\\(mrt_station\\)", "", term)
+  ) %>%
+  select(station, estimate)
+
+# Top 10
+top10 <- station_fe %>%
+  arrange(desc(estimate)) %>%
+  slice(1:10)
+
+# Bottom 10
+bottom10 <- station_fe %>%
+  arrange(estimate) %>%
+  slice(1:10)
+
+plot_station <- bind_rows(
+  top10,
+  bottom10
+)
+
+f8 <- ggplot(plot_station,
+       aes(x = reorder(station, estimate),
+           y = estimate)) +
+  geom_col() +
+  coord_flip() +
+  labs(
+    title = "Top and Bottom MRT Station Fixed Effects",
+    subtitle = "Controlling for weather and time factors",
+    x = NULL,
+    y = "Station Fixed Effect"
+  ) +
+  theme_minimal(base_family = "PingFang TC")
+ggsave("Figures/figure8.png", f8, width = 8, height = 5, dpi = 300)
